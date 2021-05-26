d40d8685a58ad271ad5ef2f1dfadc037bcec5b70
Donovan Clarke <dclarke@cadent.tv>
2021-05-26 12:24:30 -0400

    chore: refactor useEffects, rely on useRef to handle set interval

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import ProgressCircle from "./patterns/progress/ProgressCircle";
 import './App.css';
 
@@ -6,37 +6,37 @@ export const App = () => {
   const [isLoading, setLoading] = useState(true);
   const [percentage, setPercentage] = useState(0);
 
-  const handleLoading = (loading) => {
-    setLoading(loading);
-  }
-  useEffect(() => {
-    setPercentage(1)
-  }, []);
-  useEffect(() => {
-    let interval = null;
+  const interval = useRef(null);
 
-    interval = setInterval(() => {
+  const start = () => {
+    if (interval.current !== null) return;
+
+    setLoading(true);
+    interval.current = setInterval(() => {
       setPercentage(percents => percents + 1)
     }, 200);
+  }
 
-    if (!isLoading) {
-      clearInterval(interval);
-    }
+  const stop = () => {
+    if (interval.current === null) return;
+    setPercentage(percentage);
+    setLoading(false);
+    clearInterval(interval.current);
+    interval.current = null;
+  }
 
+  useEffect(() => {
     if (percentage === 101) {
-      clearInterval(interval);
-      setPercentage(1);
+      setPercentage(0);
     }
-
-    return () => clearInterval(interval);
-  }, [isLoading, percentage]);
+  }, [percentage])
 
   return (
     <div className="app-container">
       <ProgressCircle percentage={percentage} loading={isLoading} />
       <div>
-        <button onClick={() => handleLoading(true)}>Start</button>
-        <button onClick={() => handleLoading(false)}>End</button>
+        <button onClick={start}>Start</button>
+        <button onClick={stop}>End</button>
       </div>
     </div>
   )
